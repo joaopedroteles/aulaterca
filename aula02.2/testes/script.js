9c16670e6d0f26bc926471bb5d7250e2b01d9189
João Pedro de Oliveira Teles <80984493+joaopedroteles@users.noreply.github.com>
2024-08-13 10:12:56 -0300

    Add files via upload

---
+++ aula02.2/testes/script.js
@@ -0,0 +1,26 @@
+function escolher() {
+    let options = Number(prompt ('Digite a opção desejada'))
+
+    switch (options) {
+        case 1: 
+        alert ("vc escolheu 1")
+        break
+
+        case 2: 
+        alert ("vc escolheu 1")
+        break
+
+        case 3: 
+        alert ("vc escolheu 1")
+        break
+
+        case 4: 
+        alert ("vc escolheu 1")
+        break
+
+        default:
+            alert('opção invalida')
+            break
+    }
+
+}
